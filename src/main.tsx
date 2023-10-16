5b407febbe2d804094a94fb9329c8c1f68e17f21
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2023-10-16 16:47:13 +0200

    connect CurrentQuestionComponent to the core-logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { initReduxStore } from "./who-wants-to-be-millionaire/store/reduxStore.ts";
+import { QuestionGatewayStub } from "./who-wants-to-be-millionaire/adapters/secondary/gateways/questionGatewayStub.ts";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const questionGateway = new QuestionGatewayStub();
+const store = initReduxStore({ questionGateway });
+
+questionGateway.question = {
+  id: "1",
+  label:
+    "What is the answer to the Ultimate Question of Life, the Universe, and Everything?",
+  possibleAnswers: {
+    A: "42",
+    B: "24",
+    C: "404",
+    D: "666",
+  },
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
--- src/who-wants-to-be-millionaire/PossibleAnswers.component.tsx
+++ src/who-wants-to-be-millionaire/PossibleAnswers.component.tsx
@@ -1,18 +1,21 @@
-export const PossibleAnswers = () => {
+import { FC } from "react";
+import { Question } from "./core-logic/use-cases/question-retrieval/question.ts";
+
+export type Props = {
+  answers: Question["possibleAnswers"];
+};
+
+export const PossibleAnswers: FC<Props> = ({ answers }) => {
   return (
     <div className="w-full justify-center grid grid-cols-2 text-white gap-4 font-mono text-sm text-left font-bold leading-6 bg-stripes-fuchsia rounded-lg">
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
-        <span className="text-orange-500">A:</span> Third-Driven Development
-      </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
-        <span className="text-orange-500">B:</span> Test-Driven Development
-      </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
-        <span className="text-orange-500">C:</span> Test-Deep Development
-      </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
-        <span className="text-orange-500">D:</span> Test-Driven Design
-      </div>
+      {Object.entries(answers).map(([answerLetter, label]) => (
+        <div
+          key={answerLetter}
+          className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900"
+        >
+          <span className="text-orange-500">{answerLetter}:</span> {label}
+        </div>
+      ))}
     </div>
   );
 };
