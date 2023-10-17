2b5e37bcc0b944f4973c6efa3ab6b19e14367562
Mathias Kurz <mdkurz@gmail.com>
2023-10-17 14:10:07 +0200

    add colors to answers after validation

--- src/who-wants-to-be-millionaire/adapters/primary/react/components/PossibleAnswers.component.tsx
+++ src/who-wants-to-be-millionaire/adapters/primary/react/components/PossibleAnswers.component.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import { AppDispatch } from "../../../../store/reduxStore.ts";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { validateAnswer } from "../../../../core-logic/use-cases/answer-validation/validateAnswer.ts";
-import { AnswerLetter, Question } from "../../../../store/appState.ts";
+import { AnswerLetter, AppState, Question } from "../../../../store/appState.ts";
 
 export type Props = {
   answers: Question["possibleAnswers"];
@@ -15,16 +15,36 @@ export const PossibleAnswers: FC<Props> = ({ answers }) => {
     return dispatch(validateAnswer(answerLetter));
   };
 
+  const validatedAnswer = useSelector((state: AppState) => state.answerValidation);
+  
+  if (validatedAnswer) return (
+    <div className="w-full justify-center grid grid-cols-2 text-white gap-4 font-mono text-sm text-left font-bold leading-6 bg-stripes-fuchsia rounded-lg">
+      {Object.entries(answers).map(([answerLetter, label]) => {
+        let style = '';
+        if (answerLetter === validatedAnswer.given && validatedAnswer.given !== validatedAnswer.correct) style = 'bg-red-500';
+        if (answerLetter === validatedAnswer.correct) style = 'bg-green-500';
+        return (
+          <div
+            key={answerLetter}
+            className={`border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900 ${style}`}
+          >
+            <span className="text-orange-500">{answerLetter}:</span> {label}
+          </div>
+      )})}
+    </div>
+  );
+
   return (
     <div className="w-full justify-center grid grid-cols-2 text-white gap-4 font-mono text-sm text-left font-bold leading-6 bg-stripes-fuchsia rounded-lg">
       {Object.entries(answers).map(([answerLetter, label]) => (
-        <div
+        <button
           key={answerLetter}
-          className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900"
+          className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900 cursor-pointer text-left"
           onClick={selectAnswer(answerLetter as AnswerLetter)}
+          type="button"
         >
           <span className="text-orange-500">{answerLetter}:</span> {label}
-        </div>
+        </button>
       ))}
     </div>
   );
